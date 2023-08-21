3b6bc9c90dfac6550276335264dedd20715fd39a
fkabigting <141104179+fkabigting@users.noreply.github.com>
2023-08-21 00:46:02 -0600

    Added new CSS

---
+++ VS Code Assignment 1/Hello_World.js
@@ -0,0 +1,8 @@
+var greeting = 'Hello, Mundo!';
+console.log (greeting);
+
+var greeting2 = 'Welcome to node.js';
+console.log (greeting2);
+
+var sentence1 = 'Learning how to debug using the VS debugger';
+console.log (sentence1);
